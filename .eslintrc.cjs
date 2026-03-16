24ba5f52e6e96d79f9f6ccf9f568ba527f59699b
empshyamjeet <empshyamjeet@gmail.com>
2026-03-16 16:21:33 +0530

    update code

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['node_modules/', 'reports/', 'test-results/'],
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+};
